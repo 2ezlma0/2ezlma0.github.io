44a81aa824871c547125cfb17ead36ceb1b3f366
Jacob <62702188+2ezlma0@users.noreply.github.com>
2023-03-05 18:56:29 +0000

    done for now :D

--- web.js
+++ web.js
@@ -1,8 +1,21 @@
 function opacity(){
   var mainT = document.getElementById("maint");
+  var steam = document.getElementById("steam");
+  var twitter = document.getElementById("twitter");
+  var yt = document.getElementById("yt");
+  var ttv = document.getElementById("ttv");
   mainT.style.opacity = 0;
+  steam.style.opacity = 0;
+  twitter.style.opacity = 0;
+  yt.style.opacity = 0;
+  ttv.style.opacity = 0;
   setTimeout(2);
   mainT.addEventListener("animationend", ()=> {
     mainT.style.opacity = 1;
+    steam.style.opacity = 1;
+    twitter.style.opacity = 1;
+    yt.style.opacity = 1;
+    ttv.style.opacity = 1;
   });
 }
+
