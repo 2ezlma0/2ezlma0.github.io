eca8fb39f692a06edc20e269df7caf539c5a58a4
Jacob <62702188+2ezlma0@users.noreply.github.com>
2023-03-05 19:08:59 +0000

    sorted fade

--- web.js
+++ web.js
@@ -4,11 +4,6 @@ function opacity(){
   var twitter = document.getElementById("twitter");
   var yt = document.getElementById("yt");
   var ttv = document.getElementById("ttv");
-  mainT.style.opacity = 0;
-  steam.style.opacity = 0;
-  twitter.style.opacity = 0;
-  yt.style.opacity = 0;
-  ttv.style.opacity = 0;
   setTimeout(2);
   mainT.addEventListener("animationend", ()=> {
     mainT.style.opacity = 1;
